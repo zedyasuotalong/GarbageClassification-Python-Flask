ccc86de546b586e7250dd7bbd308c73b1d172c7d
zedyasuotalong <823604814@qq.com>
2023-03-19 11:57:50 +0800

    update garbage by mzt

--- api/garbage.py
+++ api/garbage.py
@@ -17,10 +17,44 @@ def Garbage_showOneCategory(category_id):
     if data is None:
         return GARBAGE_INFO_NONEXISTS, None
 
-    data = Class_To_Data(data, g_o.__fields__, 1)
+    data = Class_To_Data(data, g_o.__fields__, 0)
     DEBUG(data=data)
 
     if len(data) == 0:
         return GARBAGE_INFO_NONEXISTS, None
 
     return OK, data
+
+
+def Garbage_list():
+    DEBUG(func='api/Garbage_list')
+    g_o = Garbage_operation()
+    data = g_o._all()
+
+    if data == []:
+        return OK, data
+    data = Class_To_Data(data, g_o.__fields__, 0)
+
+    DEBUG(data=data)
+    return OK, data
+
+
+def Garbage_change_info(id, dict_value):
+    DEBUG(func='api/Garbage_change_info')
+    g_o = Garbage_operation()
+    ans = g_o._update(id, dict_value)
+    return ans
+
+
+def Garbage_delete_info(id):
+    DEBUG(func='api/Garbage_delete_info')
+    g_o = Garbage_operation()
+    ans = g_o._delete(id)
+    return ans
+
+
+def Garbage_add_info(dict_value):
+    DEBUG(func='api/Garbage_add')
+    g_o = Garbage_operation()
+    ans = g_o._add(dict_value)
+    return ans
--- operation/garbage.py
+++ operation/garbage.py
@@ -6,7 +6,7 @@ from error_code import *
 class Garbage_operation():
     def __init__(self):
         DEBUG(func='Garbage_operation/__init__')
-        self.__fields__ = ['id', 'name', 'time', 'type', 'info', 'count']
+        self.__fields__ = ['id', 'name', 'category_id', 'info', 'count']
 
     def _all(self):
         DEBUG(func='Garbage_operation/_all')
@@ -19,6 +19,32 @@ class Garbage_operation():
         garbage_list = Garbages.query.filter_by(category_id=category_id)
         DEBUG(garbage_list=garbage_list)
         return garbage_list
+
+    def _add(self, dict_value):
+        DEBUG(func='Garbage_operation/_add')
+        name = dict_value['name']
+        category_id = dict_value['category_id']
+        info = dict_value['info']
+        garbage = Garbages(name=name, category_id=category_id, info=info)
+        ans = Model_add_garbage(garbage)
+        return ans
+
+    def _delete(self, id):
+        DEBUG(func='Garbage_operation/_delete')
+        data = Garbages.query.filter_by(id=id)
+        if data.first() is None:
+            return USER_ACCOUNT_NONEXISTS
+        try:
+            ans = data.delete() # ans should be 1
+        except:
+            ans = 0
+        DEBUG(update_ans=ans)
+        if ans != 1:
+            return GARBAGE_CHANGE_INFO_ERROR
+
+        ans = Model_commit()
+        DEBUG(commit_ans=ans)
+        return ans
 
     def _update(self, id, dict_value):
         DEBUG(func='Garbage_operation/_sort')
